27c12c61dc109d402e5240acdee638e3a5fc2a43
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-10-23 21:43:47 +0600

    4th done

---
+++ 4th.py
@@ -0,0 +1,18 @@
+def plusMinus(arr):
+    x,z,y=0,0,0
+    for i in range(0,len(arr)):
+        if arr[i]>0:
+            x = x + 1
+        elif arr[i]<0:
+            y = y + 1
+        else:
+            z = z + 1
+    print(x/len(arr))
+    print(y/len(arr))
+    print(z/len(arr))
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+plusMinus(arr)
